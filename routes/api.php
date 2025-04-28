37828498b70dc362b5893f4c41b05c3e899c783c
Rayan <rayanabroudj4@gmail.com>
2025-04-28 11:48:51 +0100

    Week 4 API & Tests

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\ChirpResource;
+use App\Models\Chirp;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,16 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
+| http://localhost:8080/api/chirps
 */
+
+Route::get('/chirp/{id}', function (string $id) {
+    return new ChirpResource(Chirp::findOrFail($id));
+});
+
+Route::get('/chirps', function () {
+    return ChirpResource::collection(Chirp::all());
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
